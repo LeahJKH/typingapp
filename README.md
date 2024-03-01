ed66c62ed51eb975e3b5cf404602138389ab7729
LeahYJ <leahjb06.kodehode@gmail.com>
2024-03-01 10:07:37 +0100

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+### typing game
+This is the start of the typing game. i am gonna challenge myself in making a typing game about coding. site is built on base Javascript, HTML and CSS.
+
+## changelog
+
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+const points = document.querySelector("#typing-points")
+const highscore = document.querySelector("#typing-highscore")
+const time = document.querySelector("#typing-time")
+const screenTxt = document.querySelector("#typing-text")
+const screen = document.querySelector("#typing-screen")
