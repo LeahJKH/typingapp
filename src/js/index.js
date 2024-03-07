73bafabe7d1670345d46aa6dc9d21daa50e05a93
LeahYJ <leahjb06.kodehode@gmail.com>
2024-03-07 13:22:41 +0100

    cleaning up

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ const highscore = document.querySelector("#typing-highscore")
 const time = document.querySelector("#typing-time")
 const screenTxt = document.querySelector("#typing-text")
 const screen = document.querySelector("#typing-screen")
+
