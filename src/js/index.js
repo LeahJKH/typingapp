971256caf72ef45c6e8182d7a6a4bece2be62d86
LeahYJ <leahjb06.kodehode@gmail.com>
2024-03-09 21:26:34 +0100

    added timer

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,42 @@
-import { wordField } from "./module.js"; 
+import { wordField } from "./module.js";
 
 const points = document.querySelector("#typing-points");
 const highScore = document.querySelector("#typing-highscore");
 const time = document.querySelector("#typing-time");
 const screenTxt = document.querySelector("#typing-text");
 const screen = document.querySelector("#typing-screen");
+const startGame = document.querySelector("#start-game");
 
 let newHighScore = 0;
-screenTxt.textContent = wordField[4];
+let startTime;
+let intervalId; 
+
+screenTxt.textContent = wordField[Math.floor(Math.random() * wordField.length)]; 
 
 highScore.textContent = newHighScore;
+
+startGame.addEventListener("click", function () {
+  startStop();
+  startGame.style.display = "none";
+  screenTxt.style.display = "flex";
+});
+
+function startStop() {
+  if (startTime) {
+    clearInterval(intervalId); 
+    let endTime = Date.now();
+    let difference = endTime - startTime;
+    time.innerText = 'Time: ' + (difference / 1000).toFixed(2) + 's';
+    startTime = null;
+  } else {
+    startTime = Date.now();
+    intervalId = setInterval(updateTime, 100); 
+    updateTime(); 
+  }
+}
+
+function updateTime() {
+    let currentTime = Date.now();
+    let elapsed = (currentTime - startTime) / 1000; 
+    time.innerText = 'Time: ' + Math.floor(elapsed) + 's'; 
+  }
